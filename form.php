94824fada65e5a55278418c99b7b716e203acd95
Khaoula <AlKhaoula02@gmail.com>
2019-11-14 13:20:02 +0100

    maj

--- form.php
+++ form.php
@@ -1,41 +1,76 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/style.css">
     <title>table de multiplication</title>
 
 
 </head>
+
 <body>
-    <h1> Table de multiplication </h1>
 
-<div class="contenu">
-    <form action="form.php" method="post" >
-        <select name="nombre" id="nombre">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        </select>
-        <input type="submit" value="multipliez!">
-    </form>
-</div>
-</div>
+    <div class="titre">
+        <h1> Table de multiplication </h1>
 
-<?php
-var_dump ($_POST["nombre"]);
+    </div>
 
-
-
-
-
-?>
-
-
-
-
+    <div class="contenu">
     
+        <form action="form.php" method="post">
+            <select name="nombre" id="nombre">
+                <option value="0"></option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+            </select>
+            <input type="submit" value="multipliez!">
+        </form>
+    
+    
+
+
+    <table id="table">
+        <?php
+        session_start();
+        
+         $var = (int) $_POST["nombre"];
+         $_SESSION["var"] = $var;
+        for ($i = 0; $i < 16; $i++) { 
+            ?>
+            <tr>
+                <td><?php echo $var . "*" . $i . "=" ?> </td>
+                <td><?php echo $var * $i ?></td>
+            </tr>
+
+
+            <!-- echo $var."*". $i. "=". $var * $i."<br>"; -->
+
+
+
+        <?php } 
+        
+        ?>
+    </table>
+    </div>
+
+
+
+
 </body>
+
 </html>
---
+++ revision.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Révision</title>
+</head>
+
+<body>
+    <h1>Revisez!</h1>
+    <h2>quel est le resultat de:</h2>
+    
+    <?php
+    session_start();
+    echo "revisez la table de: ". $_SESSION["var"] . ".<br>";
+    
+    $var1 = rand(0, 15);
+    
+    
+    ?>
+    <p>
+    <?php
+    echo $_SESSION["var"]." * ".$var1;
+    ?>
+    </p>
+
+    <form action="revision.php" method="post">
+        <input type="text" name = "reponse" >
+        <input type="submit">
+    </form>
+    <p>
+        votre réponse est: tru
+    </p>
+
+
+    <?php 
+    
+    $rep= (int) $_POST["reponse"];
+    if ( $rep == ($_SESSION["var"]*$var1 )){
+        echo "bonne";
+    }
+    
+
+    ?>
+    
+
+</body>
+
+</html>
